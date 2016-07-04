5fda090d625ace1d8b8e435ef26a64d84179bad7
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2016-07-04 17:19:20 -0300

    Set hours and minutes as input values

--- src/directives/timepicker.js
+++ src/directives/timepicker.js
@@ -9,10 +9,41 @@
   function buTimepicker($document) {
     var directive = {
       restrict: 'E',
-      template: '<input><input>'
+      require: '?ngModel',
+      template: '<input ng-model="hours"><input ng-model="minutes">',
+      link: link
     };
 
     return directive;
+
+    ///
+
+    function link(scope, element, attrs, ngModel) {
+      if (!ngModel) {
+        return;
+      }
+
+      var timepicker = angular.element(element[0]);
+      var inputs = element.find('input');
+      var hoursInput = inputs.eq(0);
+
+      ngModel.$formatters.push(modelToView);
+      ngModel.$render = render;
+
+      ///
+
+      function modelToView(modelValue) {
+        return {
+          hours: modelValue.getHours(),
+          minutes: modelValue.getMinutes()
+        };
+      }
+
+      function render() {
+        scope.hours = ngModel.$viewValue.hours;
+        scope.minutes = ngModel.$viewValue.minutes;
+      }
+    }
   }
 
 })(angular);
--- tests/directives/timepickerSpec.js
+++ tests/directives/timepickerSpec.js
@@ -15,7 +15,7 @@ describe('bulma.directives.timepicker', function() {
     $rootScope = _$rootScope_;
     $document = _$document_;
 
-    $rootScope.time = newTime(12);
+    $rootScope.time = newTime(12, 34);
     element = $compile(
       '<bu-timepicker ng-model="time"></bu-timepicker>'
     )($rootScope);
@@ -24,17 +24,41 @@ describe('bulma.directives.timepicker', function() {
     $rootScope.$digest();
   }));
 
-  function newTime(hours) {
+  function newTime(hours, minutes) {
     var time = new Date();
     time.setHours(hours);
-    time.setMinutes(0);
+    time.setMinutes(minutes);
     time.setSeconds(0);
     time.setMilliseconds(0);
     return time;
   }
 
+  function getModelState() {
+    return [
+      $rootScope.time.getHours(),
+      $rootScope.time.getMinutes()
+    ];
+  }
+
+  function getViewState(element) {
+    return [
+      inputs.eq(0).val(),
+      inputs.eq(1).val()
+    ];
+  }
+
   it('Has two input fields', function() {
     expect(inputs.size()).to.equal(2);
+  });
+
+  it('Shows the hour of the sent date in the first input field', function() {
+    expect(getModelState()[0]).to.equal(12);
+    expect(getViewState()[0]).to.equal('12');
+  });
+
+  it('Shows the minute of the sent date in the second input field', function() {
+    expect(getModelState()[1]).to.equal(34);
+    expect(getViewState()[1]).to.equal('34');
   });
 
 });
